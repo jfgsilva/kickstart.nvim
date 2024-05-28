e0e24395c434bbd067f02973a2c2ca4963bf74c2
Joao Filipe Silva <joao.fcg.silva@gmail.com>
2024-05-28 10:24:20 +0100

    added symlinks for nvim configs

--- lua/custom/plugins/filetree.lua
+++ lua/custom/plugins/filetree.lua
@@ -1,12 +1,1 @@
-return {
-  "nvim-neo-tree/neo-tree.nvim",
-  version = "*",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-    "MunifTanjim/nui.nvim",
-  },
-  config = function ()
-    require('neo-tree').setup {}
-  end,
-}
+/Users/jsilva/dotfiles/nvim-plugins/filetree.lua
