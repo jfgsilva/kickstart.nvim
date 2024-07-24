2f42e0c41b5785cf8e9986831c6cc128324ee463
Joao Filipe Silva <joao.fcg.silva@gmail.com>
2024-07-24 16:00:58 +0100

    debbuger and tabstop

--- init.lua
+++ init.lua
@@ -104,6 +104,7 @@ vim.opt.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
 vim.opt.foldlevelstart = 1
 vim.opt.foldcolumn = '0'
 vim.opt.foldenable = false
+vim.opt.tabstop = 4
 -- vim.opt.foldnestmax = 4
 -- vim.opt.foldtext = ''
 
---
+++ lua/custom/plugins/nvim-dap.lua
@@ -0,0 +1,1 @@
+/Users/jsilva/dotfiles/nvim-plugins/nvim-dap.lua
